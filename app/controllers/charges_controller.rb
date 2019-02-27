d4849a77d99bd368dc141c6f88b550e07641ee06
Kashyap Vadrevu <kashyap.vad@gmail.com>
2019-02-26 23:00:42 -0600

    added subscrption plan for cat

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,18 +9,19 @@ class ChargesController < ApplicationController
     customer = Stripe::Customer.create({
                                            email: params[:stripeEmail],
                                            source: params[:stripeToken],
+                                           plan: "plan_EbXOWerXM2xgNt"
                                        })
 
-    charge = Stripe::Charge.create({
-                                       customer: customer.id,
-                                       amount: product.price_in_cents,
-                                       description: 'Rails Stripe customer',
-                                       currency: 'usd',
-                                   })
+    #charge = Stripe::Charge.create({
+    #                                   customer: customer.id,
+    #                                   amount: product.price_in_cents,
+    #                                   description: 'Rails Stripe customer',
+    #                                   currency: 'usd',
+    #                               })
 
     purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken],
-                               amount: product.price_in_cents, description: charge.description,
-                               currency: charge.currency, customer_id: customer.id, product_id: product.id)
+                               amount: product.price_in_cents, description: product.description,
+                               currency: "usd", customer_id: customer.id, product_id: product.id)
     redirect_to purchase
 
   rescue Stripe::CardError => e
